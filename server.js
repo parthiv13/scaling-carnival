84e916a51f60b8f3363c9a6ddae0f8beae278576
parthiv13 <kparthiv13@gmail.com>
2018-11-21 11:20:43 +0530

    Scaling-Carnival Complete

--- server.js
+++ server.js
@@ -25,28 +25,27 @@ csv()
         }
         console.log(obj);
         doc.setData(obj);
+
+        try {
+            doc.render()
+        }
+        catch (error) {
+            var e = {
+                message: error.message,
+                name: error.name,
+                stack: error.stack,
+                properties: error.properties,
+            }
+            console.log(JSON.stringify({ error: e }));
+
+            throw error;
+        }
+        var buf = doc.getZip().generate({ type: 'nodebuffer' });
+
+        fs.writeFileSync(path.resolve(__dirname, 'output.docx'), buf);
     });
 
 var randomJson = {
     XXX: 'lololo',
     BBB: 'GE'
 }
-
-try {
-    doc.render()
-}
-catch (error) {
-    var e = {
-        message: error.message,
-        name: error.name,
-        stack: error.stack,
-        properties: error.properties,
-    }
-    console.log(JSON.stringify({ error: e }));
-
-    throw error;
-}
-
-var buf = doc.getZip().generate({ type: 'nodebuffer' });
-
-fs.writeFileSync(path.resolve(__dirname, 'output.docx'), buf);
